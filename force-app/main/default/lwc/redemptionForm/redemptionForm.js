53276d3daa90dbba284dc6021e74d738a7e0cb10
victorino3 <victorinogomes1@gmail.com>
2025-04-19 20:13:15 +0100

    Sprint 3, review and deploy

--- force-app/main/default/lwc/redemptionForm/redemptionForm.js
+++ force-app/main/default/lwc/redemptionForm/redemptionForm.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import createRedemption from '@salesforce/apex/RedemptionController.createRedemption';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class RedemptionForm extends LightningElement {}
+export default class RedemptionForm extends LightningElement {
+    @api selectedReward;
+    customerId = '';
+    isSubmitting = false;
+
+    handleCustomerChange(event) {
+        this.customerId = event.target.value;
+    }
+
+
+    async handleSubmit() {
+        this.isSubmitting = true;
+        try {
+            await createRedemption({ 
+                customerId: this.customerId, 
+                rewardId: this.selectedReward.Id 
+            });
+
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Success',
+                message: `Redemption created for ${this.selectedReward.Name}`,
+                variant: 'success'
+            }));
+
+            this.customerId = '';
+        } catch (error) {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error creating redemption',
+                message: error.body.message,
+                variant: 'error'
+            }));
+        } finally {
+            this.isSubmitting = false;
+        }
+    }
+
+    get isRedeemDisabled() {
+        return this.isSubmitting || !this.customerId;
+    }
+}
--- force-app/main/default/lwc/rewardCatalog/rewardCatalog.js
+++ force-app/main/default/lwc/rewardCatalog/rewardCatalog.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
 
-export default class RewardCatalog extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import getAvailableRewards from '@salesforce/apex/RewardController.getAvailableRewards';
+
+export default class RewardCatalog extends LightningElement {
+    rewards;
+    error;
+
+    @wire(getAvailableRewards)
+    wiredRewards({ data, error }) {
+        if (data) {
+            this.rewards = data;
+        } else if (error) {
+            this.error = error.body.message;
+        }
+    }
+
+    handleSelect(reward) {
+        // Emit reward to parent (e.g. RedemptionForm)
+        const event = new CustomEvent('rewardselected', {
+            detail: reward
+        });
+        this.dispatchEvent(event);
+    }
+}
