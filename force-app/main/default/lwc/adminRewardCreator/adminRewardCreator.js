3712e607f3f652923fee8eab01a603b4d529ac97
victorino3 <victorinogomes1@gmail.com>
2025-04-24 16:06:23 +0100

    Sprint 5, add configured mulesoft AML file, apex call and admin panel

--- force-app/main/default/lwc/adminRewardCreator/adminRewardCreator.js
+++ force-app/main/default/lwc/adminRewardCreator/adminRewardCreator.js
@@ -1,3 +1,66 @@
 import { LightningElement } from 'lwc';
+import fetchRewardsFromMuleSoft from '@salesforce/apex/MuleSoftAPIController.fetchRewardsFromMuleSoft';
+import fetchCustomersFromMuleSoft from '@salesforce/apex/MuleSoftAPIController.fetchCustomersFromMuleSoft';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class AdminRewardCreator extends LightningElement {}
+export default class AdminPanel extends NavigationMixin(LightningElement) {
+    mode = 'form';
+    muleAction = '';
+    rewards = [];
+    customers = [];
+
+    handleModeChange(event) {
+        this.mode = event.target.value;
+        this.muleAction = '';
+    }
+
+    handleMuleActionChange(event) {
+        this.muleAction = event.target.value;
+        if (this.muleAction === 'rewards') {
+            this.getRewards();
+        } else if (this.muleAction === 'customers') {
+            this.getCustomers();
+        }
+    }
+
+    getRewards() {
+        fetchRewardsFromMuleSoft()
+            .then(result => {
+                this.rewards = result;
+            })
+            .catch(error => {
+                console.error('Error fetching rewards:', error);
+            });
+    }
+
+    getCustomers() {
+        fetchCustomersFromMuleSoft()
+            .then(result => {
+                this.customers = result;
+            })
+            .catch(error => {
+                console.error('Error fetching customers:', error);
+            });
+    }
+
+    handleViewRewards() {
+        this.navigateToObject('Reward__c');
+    }
+
+    handleViewCustomers() {
+        this.navigateToObject('Customer__c');
+    }
+
+    navigateToObject(objectApiName) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName,
+                actionName: 'list'
+            },
+            state: {
+                filterName: 'Recent'
+            }
+        });
+    }
+}
